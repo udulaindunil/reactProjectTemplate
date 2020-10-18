e7b7a494bf14706c77918bad0897bac5c9d02c00
udulaindunil <udulaindunil@gmail.com>
2020-10-18 23:32:30 +0530

    template

--- src/components/App.js
+++ src/components/App.js
@@ -1,88 +1,19 @@
 import React, { Component } from 'react';
-import Projects from './Projects'
-import SocialProfile from './SocialProfile';
-import Title from './Title';
 
-
-
-import logo from '../logo.svg';
-import '../App.css';
-
-
-class App extends  Component {
-  state ={displayBio:false};
-
-  toggleDisplayBio=()=>{
-      this.setState({displayBio: !this.state.displayBio})
-  }
-
-  render(){
-    return(
-
-      <div>
-            <h1 class="h1">
-                Hello!
-            </h1>
-            
-            <p>
-                My name is Udula Indunl Sensly
-            </p>
-            <Title/>
-            <p>Im always looking foeward to working on meaningful projects.</p>
-
-            
-
-            { this.state.displayBio ? (
-                <div>
-                    <p>
-                        I live in San Francisco and code every day.
-                    </p>
-
-                    <p>My Favourite language is JavaScript and I think React.js is awesome</p>
-
-                    <p>Besides coding , I also like music and ramen!</p>
-                    
-                    
-
-                    <button onClick={this.toggleDisplayBio}>Show less</button>
-                </div>
-            ): (
-                <div>
-                    <button onClick={this.toggleDisplayBio}>Read more</button>
-
-                </div>
-
-
-            )}
-
-            {/*{bio}*/}
+class App extends Component{
+    
+    
+    render(){
+        return(
 
             <div>
+                <h2>
+                React App
+                </h2>
 
-                <Projects/>
             </div>
-
-            <div>
-
-                <SocialProfile/>
-            </div>
-
-            <div>
-
-                {this.state.displayBio ?(<Title/>):null}
-            {/* <Title/> */}
-            </div>
-
-
-
-        </div>
-
-    )
-  }
-
+        );
+    }
 }
-
-
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,40 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import {Router , Switch , Route } from 'react-router-dom';
-import createBrowserHistroy from 'history/createBrowserHistory'
-import Jokes from './components/Jokes';
-import Header from './components/Header'
-import MusicMaster from './projects/musicmaster';
-import EvensOrOdd from'./projects/evens-or-odd';
-import Reaction from'./projects/reaction';
 
-
-ReactDOM.render(
-  <Router history={createBrowserHistroy()}>
-    <Switch>
-      <Route exact={true} path='/' render={() => <Header><App/></Header>} />
-      <Route path='/jokes' render={() => <Header><Jokes/></Header>}/>
-
-      <Route path='/music-master' render={() => <Header><MusicMaster/></Header>}/>
-      <Route path='/even-or-odds' render={() => <Header><EvensOrOdd/></Header>}/>
-      <Route path='/reaction' render={() => <Header><Reaction/></Header>}/>
-    </Switch> 
-  </Router>,
+ReactDOM.render(<App/>,
   document.getElementById('root')
 );
 
  
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
